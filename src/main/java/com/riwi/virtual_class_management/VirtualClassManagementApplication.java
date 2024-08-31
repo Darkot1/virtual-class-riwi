e2c39aa40b70df2d85e992ec8a87abc9a5e86bb2
Darkot1 <sebastian.serna.1998@gmail.com>
2024-08-31 14:47:09 -0500

    first commit

---
+++ src/main/java/com/riwi/virtual_class_management/VirtualClassManagementApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.virtual_class_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualClassManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualClassManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/riwi/virtual_class_management/VirtualClassManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.virtual_class_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualClassManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

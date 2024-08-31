abe946c0d6779e978aba2752297d5a60b566ec50
Darkot1 <sebastian.serna.1998@gmail.com>
2024-08-31 15:17:09 -0500

    add class

---
+++ src/main/java/com/riwi/virtual_class_management/entities/Class.java
@@ -0,0 +1,42 @@
+package com.riwi.virtual_class_management.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "classes")
+public class Class {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private boolean active = true;
+
+    @ManyToMany
+    @JoinTable(
+            name = "student_class",
+            joinColumns = @JoinColumn(name = "class_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id")
+    )
+    private List<Student> students = new ArrayList<>();
+
+    @OneToMany(mappedBy = "class", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Lesson> lessons = new ArrayList<>();
+
+
+}
---
+++ src/main/java/com/riwi/virtual_class_management/entities/Lesson.java
@@ -0,0 +1,38 @@
+package com.riwi.virtual_class_management.entities;
+
+import com.riwi.virtual_class_management.enums.MultimediaType;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "lessons")
+public class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private MultimediaType multimediaType;
+
+    @Column(nullable = false)
+    private boolean active = true;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_id", nullable = false)
+    private Class classEntity;
+
+
+}
+
